62895e750df3d71671f74cc5f398f0ed90112e6d
Abu Jaher Rakib <majrakib.hatiya.bd@gmail.com>
2025-02-17 21:24:29 +0600

    change

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let a=50;
-let b=30;
-if(a>b){
-    console.log("a is gater than b");
-}else if(b>a){
-    console.log("print b is gater than a");
+
+if(a>0){
+    console.log("a is gater than 0");
+}else if(){
+    console.log(" a is less than 0");
 }else{
 console.log("the number is zero")
 }
